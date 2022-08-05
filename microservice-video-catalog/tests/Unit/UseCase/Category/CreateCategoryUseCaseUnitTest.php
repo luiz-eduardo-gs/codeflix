fcd9f0dc06a585f0e9d380da51b24cf9e47594ee
Luiz Eduardo <gs.luizeduardo00@gmail.com>
2022-08-05 18:12:04 -0300

    fix: category use case tests

--- microservice-video-catalog/tests/Unit/UseCase/Category/CreateCategoryUseCaseUnitTest.php
+++ microservice-video-catalog/tests/Unit/UseCase/Category/CreateCategoryUseCaseUnitTest.php
@@ -2,29 +2,39 @@
 
 namespace Tests\Unit\UseCase\Category;
 
+use Core\Domain\Entity\Category;
 use Core\Domain\Repository\CategoryRepositoryInterface;
 use Core\UseCase\Category\CreateCategoryUseCase;
+use Core\UseCase\DTO\Category\CreateCategoryInputDto;
+use Core\UseCase\DTO\Category\CreateCategoryOutputDto;
 use PHPUnit\Framework\TestCase;
 use Mockery;
-use stdClass;
+use Ramsey\Uuid\Uuid;
 
 class CreateCategoryUseCaseUnitTest extends TestCase
 {
     public function testCreateNewCategory()
     {
-        // $this->entityMock = Mockery::mock(Category::class, [
-        //     '1',
-        //     'category name'
-        // ]);
+
+        $uuid = (string) Uuid::uuid4()->toString();
+
+        $this->entityMock = Mockery::mock(Category::class, [
+            $uuid,
+            'category name',
+        ]);
 
         /** @var \Mockery\MockInterface|\Mockery\LegacyMockInterface */
         $this->repositoryMock = Mockery::mock(CategoryRepositoryInterface::class);
-        $this->repositoryMock->shouldReceive('insert');
+        $this->repositoryMock->shouldReceive('insert')->andReturn($this->entityMock);
+
+        $this->inputDtoMock = Mockery::mock(CreateCategoryInputDto::class, [
+            'category name'
+        ]);
 
         $useCase = new CreateCategoryUseCase($this->repositoryMock);
-        $useCase->execute();
+        $useCaseResponse = $useCase->execute($this->inputDtoMock);
 
-        $this->assertTrue(true);
+        $this->assertInstanceOf(CreateCategoryOutputDto::class, $useCaseResponse);
 
         Mockery::close();
     }
